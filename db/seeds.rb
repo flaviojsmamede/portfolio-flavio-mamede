7313e2344196f8ddd1df570a907ab5fd13942a85
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-19 16:54:45 +0100

    adding images to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,22 @@ puts "Cleaning database..."
 Project.destroy_all
 puts "Creating projects..."
 
-project1 = Project.create(title: "Project 1", description: "This is the first project")
-project2 = Project.create(title: "Project 2", description: "This is the second project")
-project3 = Project.create(title: "Project 3", description: "This is the third project")
+project1 = Project.create(
+  title: "Project 1",
+  description: "This is the first project"
+  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+)
+
+project2 = Project.create(
+  title: "Project 2",
+  description: "This is the second project"
+  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+)
+
+project3 = Project.create(
+  title: "Project 3",
+  description: "This is the third project"
+  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+)
 
 puts "All done!"
