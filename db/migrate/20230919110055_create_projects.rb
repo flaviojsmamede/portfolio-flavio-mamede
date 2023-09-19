e877dda88226b972e93a8bccbcf03b6bb524b7af
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-19 12:02:28 +0100

    created the projects model

---
+++ db/migrate/20230919110055_create_projects.rb
@@ -0,0 +1,14 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.date :start_date
+      t.date :end_date
+      t.text :description
+      t.string :images
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
