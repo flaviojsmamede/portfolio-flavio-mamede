bf4c2ec46a02dba85e1e46c0bf38fd00e17546af
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-20 11:50:41 +0100

    changing images for the projects and starting show

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,22 @@ puts "Cleaning database..."
 Project.destroy_all
 puts "Creating projects..."
 
-project1 = Project.create(
+care = Project.create(
   title: "2Care",
   description: "This is the first project",
-  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  images: "https://images.pexels.com/photos/3791664/pexels-photo-3791664.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 )
 
-project2 = Project.create(
+sandspot = Project.create(
   title: "SandSpot",
   description: "This is the second project",
-  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  images: "https://images.pexels.com/photos/130879/pexels-photo-130879.jpeg?auto=compress&cs=tinysrgb&w=1600"
 )
 
-project3 = Project.create(
+apastelaria = Project.create(
   title: "aPastelaria",
   description: "This is the third project",
-  images: "https://images.pexels.com/photos/18332033/pexels-photo-18332033/free-photo-of-snow-sea-road-landscape.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  images: "https://images.pexels.com/photos/2573870/pexels-photo-2573870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 )
 
 puts "All done!"
